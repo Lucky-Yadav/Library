a25b232aeb178743c74d69d7c356ee67b13ee155
Luckyyadav420 <97690042+Lucky-Yadav@users.noreply.github.com>
2022-11-18 21:04:34 +0530

    update navbar, index and css

---
+++ components/navbar.js
@@ -0,0 +1,16 @@
+function navbar() {
+  return `<div class="fixed_header">
+    <div id="navbar">
+        <div class="logo">
+        <a href="./index.html"><p>Home</p></a>            
+        <a href="./index.html"><p>Dashboard</p></a>            
+        </div>
+        <div class="options">
+            <a href="./showrecipie.html"><p>My Books</p></a> 
+            <a href="./search.html"><p>Bookmarks</p></a>            
+         </div>
+        </div>
+    </div>`;
+}
+
+export default navbar;
